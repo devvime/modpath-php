9946cea7bff1b0876cd5b607c627bbfd8791c72f
devvime <dihapp@outlook.com>
2025-06-04 21:17:57 -0300

    fix: template engine

--- src/View/View.php
+++ src/View/View.php
@@ -21,18 +21,15 @@ class View
 
     protected static function compile(string $template): string
     {
-        // {{ $var }} => php htmlspecialchars($var)
         $template = preg_replace('/\{\{\s*(.+?)\s*\}\}/', '<?= htmlspecialchars($1) ?>', $template);
-
-        // <if(condition)> => php if(condition):
         $template = preg_replace('/<if\((.+?)\)>/', '<?php if ($1): ?>', $template);
         $template = preg_replace('/<elseif\((.+?)\)>/', '<?php elseif ($1): ?>', $template);
         $template = str_replace('<else>', '<?php else: ?>', $template);
         $template = str_replace('<endif>', '<?php endif; ?>', $template);
-
-        // <loop($items as $item)> => php foreach($items as $item):
         $template = preg_replace('/<loop\((.+?)\)>/', '<?php foreach ($1): ?>', $template);
         $template = str_replace('<endloop>', '<?php endforeach; ?>', $template);
+        $template = preg_replace('/<for\((.+?)\)>/', '<?php for ($1): ?>', $template);
+        $template = str_replace('<endfor>', '<?php endfor; ?>', $template);
 
         return $template;
     }
